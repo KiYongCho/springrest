ebbf9b87cde3fb377cf5bba8523e4e37c8cffd15
realchoky <realchoky@gmail.com>
2024-08-14 11:24:34 +0900

    123

--- src/main/java/springrest/controller/RestBoardController.java
+++ src/main/java/springrest/controller/RestBoardController.java
@@ -18,22 +18,32 @@ import springrest.service.RestBoardService;
 
 @RestController
 public class RestBoardController {
-
-	@Autowired
-	private RestBoardService restBoardService; 
 	
+	@Autowired
+	private RestBoardService restBoardService;
+	
+	// GET /board ��û�ϸ� RestBoard���� ��� �ִ� List�� ������������͸� JSON���� ��ȯ�ؼ� ���� 
+	// ResponseEntity : ���۵Ǵµ�����(payload) + �������������
 	@GetMapping(value="/board")
 	public ResponseEntity<List<RestBoard>> listRestBoard() throws Exception {
 		List<RestBoard> restBoardList = restBoardService.listRestBoard();
-		return ResponseEntity.ok(restBoardList);
+		return ResponseEntity.ok(restBoardList); // statusCode 200 (���� ����) 
 	}
 	
+	// GET /board/1 ��û�ϸ� RestBoard�� ������������͸� JSON���� ��ȯ�ؼ� ����
+	// ��ûURI�� {}�ȿ� ������, @PathVariable�� ����
 	@GetMapping(value="/board/{rbid}")
 	public ResponseEntity<RestBoard> getRestBoard(@PathVariable int rbid) throws Exception {
 		RestBoard restBoard = restBoardService.getRestBoard(rbid);
 		return ResponseEntity.ok(restBoard);
-	}	
+	}
 	
+	// POST /board ��û�ϸ�
+	// producer-consumer ����
+	// : �����͸� �����ϴ� ��(producer)�� �����͸� �Һ��ϴ� ��(consumer)�� �и��ϴ� ����
+	// produces : ��û�������� Ÿ���� ����
+	// consumes : ���䵥������ Ÿ���� ����
+	// @RequestBody : ��û�ٵ� �ִ� json��ü�� RestBoard��ü�� ����
 	@PostMapping(value="/board", produces = MediaType.APPLICATION_JSON_VALUE)
 	public int insertRestBoard(@RequestBody RestBoard restBoard) throws Exception {
 		return restBoardService.insertRestBoard(restBoard);
@@ -47,9 +57,34 @@ public class RestBoardController {
 	@DeleteMapping(value="/board/{rbid}")
 	public int deleteRestBoard(@PathVariable int rbid) throws Exception {
 		return restBoardService.deleteRestBoard(rbid);
-	}	
+	}
 	
-}
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
